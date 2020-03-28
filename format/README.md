ac296057c82f29a20e066dcf56b3ee62c31addb5
smy-xmu <61258173+smy-xmu@users.noreply.github.com>
2020-03-28 09:45:53 +0800

    test

---
+++ format/README.md
@@ -0,0 +1,3 @@
+# A format of socket user(by Server & Client)
+
+## copy from https://github.com/shineyr/Socket
---
+++ src/settings.h
@@ -0,0 +1,14 @@
+#ifndef _SOCKET_SETTINGS_H
+#define _SOCKET_SETTINGS_H
+
+#define SPORT       6000
+#define BACKLOG     10
+
+#define BUFF_SIZE   1024
+
+#define LISTEN_LOG  "Log/tmp.txt"
+
+typedef unsigned char   byte;
+
+#endif  // _SOCKET_SETTINGS_H
+
