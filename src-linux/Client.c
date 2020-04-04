3b3635cc8078e4b4125ea85f634237d82256fe33
enatsu <unsioer@outlook.com>
2020-04-04 21:24:57 +0800

    Useless Usage();

--- src-linux/Client.c
+++ src-linux/Client.c
@@ -7,27 +7,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc , char** argv)
+void usage();
+
+int main(int argc, char **argv)
 {
+    if (argc < 2)
+    {
+        usage();
+        return -1;
+    }
     // (1) create Server's socket
     int Csocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        // Client's socket, conn to Server
+    // Client's socket, conn to Server
 
-    if (Csocket < 0){
+    if (Csocket < 0)
+    {
         // is init failed
 
         printf("Failed Socket\n");
         exit(-1);
     }
 
-    // (2) create Server's addr connect to 
+    // (2) create Server's addr connect to
     struct sockaddr_in Saddr = {0};
     Saddr.sin_family = AF_INET;
     Saddr.sin_port = htons(CPORT);
     Saddr.sin_addr.s_addr = inet_addr(argv[1]);
 
     // (3) connect to Server
-    if ( connect(Csocket, (struct sockaddr*)&Saddr, sizeof(Saddr)) < 0){
+    if (connect(Csocket, (struct sockaddr *)&Saddr, sizeof(Saddr)) < 0)
+    {
         // is conn failed
 
         printf("Failed connect\n");
@@ -40,10 +49,11 @@ int main(int argc , char** argv)
      */
 
     // (4) loop to send infomation
-    int     isExit = 0;
-    byte    recvBUFF[BUFF_SIZE];
-    byte    sendBUFF[BUFF_SIZE];
-    while(!isExit && fgets(sendBUFF, BUFF_SIZE, stdin) != NULL){
+    int isExit = 0;
+    byte recvBUFF[BUFF_SIZE];
+    byte sendBUFF[BUFF_SIZE];
+    while (!isExit && fgets(sendBUFF, BUFF_SIZE, stdin) != NULL)
+    {
         isExit = strstr(sendBUFF, "\\exit");
         // if (isExit) *strstr(sendBUFF, "\\exit")='\0';
 
@@ -55,4 +65,10 @@ int main(int argc , char** argv)
     close(Csocket);
 
     return 0;
+}
+
+void usage()
+{
+    printf("Usage: \n");
+    printf("Client [ip_addr]\n");
 }
